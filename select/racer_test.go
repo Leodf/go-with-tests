d689ce94b6b82f800f632768f725cac58059ffe9
Leonardo de Faveri <faver_i@hotmail.com>
2026-03-16 08:25:45 -0300

    create a cofigurableRacer to determine the waiting time

--- select/racer_test.go
+++ select/racer_test.go
@@ -20,20 +20,24 @@ func TestRacer(t *testing.T) {
 		fastUrl := fastServer.URL
 
 		want := fastUrl
-		got, _ := Racer(slowUrl, fastUrl)
+		got, err := Racer(slowUrl, fastUrl)
+
+		if err != nil {
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
 	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := makeDelayedServer(11 * time.Second)
-		serverB := makeDelayedServer(12 * time.Second)
+		serverA := makeDelayedServer(25 * time.Millisecond)
+		serverB := makeDelayedServer(25 * time.Millisecond)
 
 		defer serverA.Close()
 		defer serverB.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Errorf("expected an error but didn't get one")
